6772c43fada9ea82e886c20fb37b8a0244e69287
Roland Thomas <roland@rolandthomasjr.com>
2023-02-11 01:17:41 -0500

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 # Exports
 set --export EDITOR "vim"
 set --export PF_INFO "ascii title os host kernel uptime pkgs memory wm shell editor palette"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set --export MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set --export QT_QPA_PLATFORMTHEME "gtk2"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
