d25a67df953a60469bce68d91cb48614ce2526e8
Roland Thomas <roland@rtj.dev>
2023-06-24 17:44:14 -0400

    Add pactl automation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,15 @@ set --export MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set --export QT_QPA_PLATFORMTHEME "gtk2"
 
 if status is-interactive
-
-    # Commands to run in interactive sessions can go here
 end
 
 # Start X at login
 if status --is-login
+  while pactl load-module module-detect
+  end
+  if not pactl set-default-sink alsa_output.0.hdmi-stereo
+    pactl set-default-sink alsa_output.1.hdmi-stereo
+  end
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
     exec startx -- -keeptty
   end
